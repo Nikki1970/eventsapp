a9bc605b6e0d5a6df0cc6e6c027222e020100a23
Nikki1970 <sagalanikhilesh99@gmail.com>
2021-04-13 11:10:21 +0530

    use Timestamped model

--- eventapp/models.py
+++ eventapp/models.py
@@ -4,15 +4,14 @@ from django.contrib.auth.models import User
 from taggit.managers import TaggableManager
 from datetime import date
 from django.utils import timezone
+from model_utils.models import TimeStampedModel
 
-class Event(models.Model):
+class Event(TimeStampedModel):
     title = models.CharField(max_length = 12,help_text="Event Name")
     place = models.ForeignKey(Place,on_delete=models.SET_NULL,null=True,blank=True)
     host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     tags = TaggableManager()
     eventtime = models.DateTimeField(default=timezone.now,blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    modified_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return '{0} {1}'.format(self.title,self.host)
