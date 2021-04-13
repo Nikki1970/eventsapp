e1a97a0c2219c87c45eee2d4074dbe11f7943574
Nikki1970 <sagalanikhilesh99@gmail.com>
2021-04-13 11:06:14 +0530

    Created index page

--- eventapp/views.py
+++ eventapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from eventapp.models import Event
 # Create your views here.
+
+
+def index(request):
+    total = Event.objects.all().count()
+    context = {'total': total,}
+    return render(request, 'index.html', context=context)
