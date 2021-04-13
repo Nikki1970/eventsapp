b693423b61d2af4692b075d5d95b40cd0b8aa9dd
Nikki1970 <sagalanikhilesh99@gmail.com>
2021-04-13 14:25:07 +0530

    create form for entering new event

--- eventapp/urls.py
+++ eventapp/urls.py
@@ -4,4 +4,5 @@ urlpatterns = [
     path('', views.index, name='eventindex'),
     path('listofevents/',views.EventListView.as_view(),name="list-of-events"),
     path('event/<int:pk>/',views.EventDetailView.as_view(),name="event-detail"),
+    path('event/new/',views.event_new,name='new_event')
 ]
--- eventapp/views.py
+++ eventapp/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from eventapp.models import Event
 from django.views import generic
+from .forms import EventForm
 # Create your views here.
 
 
@@ -14,3 +15,18 @@ class EventListView(generic.ListView):
 
 class EventDetailView(generic.DetailView):
     model = Event
+
+def new_event(request):
+    form = EventForm
+    return render(request,)
+
+def event_new(request):
+    if request.method == "POST":
+        form = EventForm(request.POST)
+        if form.is_valid():
+            event = form.save(commit=False)
+            event.save()
+            return redirect('list-of-events')
+    else:
+        form = EventForm()
+    return render(request, 'eventapp/event_edit.html', {'form': form})
